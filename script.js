386fc0fc5ea358d683e20ff90dbfab4593d1feb6
Ammo <thefriendscorporations@gmail.com>
2026-04-28 15:45:58 +0700

    Implement splash screen loading effect
    Added a splash screen loading effect on window load.

--- script.js
+++ script.js
@@ -4,6 +4,12 @@
     import { AMOGENZ_DB_SHOROF } from './amogenzdb-shorof.js'; 
     import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
     import { getDatabase, ref, push, onValue, remove, query, limitToLast, set, get, increment } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+// loading 
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    document.getElementById('splash-screen').classList.add('hide');
+  }, 1700);
+});
 
     // --- 1. CONFIG ---
     const firebaseConfig = { apiKey: "AIzaSyBDyEfe83-_CzRchqcO_lLnuO6Rg9_AF_8", authDomain: "amogenz.firebaseapp.com", databaseURL: "https://amogenz-default-rtdb.asia-southeast1.firebasedatabase.app", projectId: "amogenz", storageBucket: "amogenz.firebasestorage.app", messagingSenderId: "864003468268", appId: "1:864003468268:web:7c861806529a0dacd66ec9" };
